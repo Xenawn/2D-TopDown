780b531e9a33bd332d2e3700d949185482435255
Aephok <codeparadoxist@gmail.com>
2025-02-18 21:58:37 +0900

    Initial commit

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+      
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movment(movementDirection);
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movment(Vector2 direction)
+    {
+        direction = direction * 5;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+    }
+
+
+
+   
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : BaseController
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+
+
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        movementDirection = new Vector2 (horizontal, vertical).normalized;
+
+      
+      
+    }
+}
+
